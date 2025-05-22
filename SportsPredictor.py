54fb799817fc5998445963b96b78af6ca62d0b8c
Tom Adoni <80783455+tomadoni@users.noreply.github.com>
2025-05-22 17:35:45 -0400

    Update SportsPredictor.py

--- SportsPredictor.py
+++ SportsPredictor.py
@@ -259,7 +259,7 @@ if home_team != away_team:
         home_stats = team_stats[team_stats["Team"] == home_team][selected_raw_features].values[0]
         away_stats = team_stats[team_stats["Team"] == away_team][selected_raw_features].values[0]
         diff_vector = home_stats - away_stats
-        clipped_vector = np.clip(diff_vector, -1.5, 1.5)  # Clamp stat diffs
+        clipped_vector = np.clip(diff_vector, -2.5, 2.5)  # Clamp stat diffs
 
         # Scale and append home field indicator
         diff_scaled = scaler.transform([clipped_vector])[0]
