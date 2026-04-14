a4a0a09de501563d4945d56d445ffaecc2be288f
Tom Adoni <80783455+tomadoni@users.noreply.github.com>
2026-04-14 11:25:20 -0400

    Update SportsPredictor.py

--- SportsPredictor.py
+++ SportsPredictor.py
@@ -1302,7 +1302,7 @@ def build_world_cup_power_ratings(team_stats: pd.DataFrame) -> pd.DataFrame:
         "goal_diff": 0.18,
         "clean_sheets": 0.09,
         "recent_form_points_5": 0.19,
-        "fifa_rank_inverse": 1.20,
+        "fifa_rank_inverse": 2.20,
     }
 
     # fifa ranking strength
